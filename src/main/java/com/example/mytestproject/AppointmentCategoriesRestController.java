10ca1c7544b407969944364a41bd73114149e55d
Malek <malekaltwoqi@gmail.com>
2024-02-13 17:29:42 +0300

    add new appoitment rest controller

---
+++ src/main/java/com/example/mytestproject/AppointmentCategoriesRestController.java
@@ -0,0 +1,15 @@
+package com.example.mytestproject;
+import  org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+
+public class AppointmentCategoriesRestController {
+    public AppointmentCategoriesRestController(){
+
+    }
+    @GetMapping("/appointment-categories")
+    public void getAppointmentCategories(@PathVariable long doctorId){
+
+    }
+}
