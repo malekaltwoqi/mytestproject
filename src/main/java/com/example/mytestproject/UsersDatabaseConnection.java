a5b5473e015ce55ea647c17a13b47311581bcdd0
Malek <malekaltwoqi@gmail.com>
2024-02-13 17:49:19 +0300

    Adding Database connction

---
+++ src/main/java/com/example/mytestproject/UsersDatabaseConnection.java
@@ -0,0 +1,21 @@
+package com.example.mytestproject;
+
+public class UsersDatabaseConnection {
+    public void connect(){
+        System.out.println("Connected ");
+    }
+    public void disconnect(){
+        System.out.println("disconnect ");
+    }
+    public void getUser(){
+        System.out.println("getUser ");
+    }
+    public void updateUser(){
+        System.out.println("updateUser ");
+    }
+    public void deleteUser(){
+        System.out.println("deleteUser ");
+    }
+    public void findById(){
+}
+    }
